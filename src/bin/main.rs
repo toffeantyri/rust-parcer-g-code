c0bf1a5e28c6532084654f23c804755d0d638df0
toffeantyri <toffeantyri@gmail.com>
2026-06-02 16:27:57 +0500

    add file with real g-code, read file in main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,14 @@
+use std::fs;
 //use code_parser::{lexer, parser, formatter};
 
 fn main() {
     println!("🚀 Запуск парсера и форматтера...");
 
-    let input = "пример кода для разбора";
+    let input_content = fs::read_to_string("input_code.txt").expect("Error reading file");
+    println!("{}", input_content);
 
     // Пока используем заглушки — нужно убедиться, что модули экспортируют эти функции
-    let tokens = lexer::lex(input);
+    let tokens = lexer::lex(&input_content);
     let ast = parser::parse(&tokens);
     let formatted = formatter::format(&ast);
 
@@ -16,7 +18,9 @@ fn main() {
 // Временные заглушки, если в библиотеке нет реализации
 // Уберите это, когда реализуете реальные модули
 mod lexer {
+
     pub fn lex(input: &str) -> Vec<String> {
+        //let lexer = Lexer::new(input.to_string());
         vec![format!("LEXED({})", input)]
     }
 }
