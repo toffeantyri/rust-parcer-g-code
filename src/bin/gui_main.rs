374a336dfe9ce26d34e90343ec535e078c7cc6fc
toffeantyri <toffeantyri@gmail.com>
2026-06-09 18:52:30 +0500

    Окно открывается максимизированным (fullscreen)
    - gui_main.rs: NativeOptions.viewport.with_maximized(true)

--- src/bin/gui_main.rs
+++ src/bin/gui_main.rs
@@ -1,9 +1,13 @@
 //! Точка входа GUI-приложения (egui)
 
 use code_parser::interfaces::gui::GCodeApp;
+use eframe::egui;
 
 fn main() -> eframe::Result {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_maximized(true),
+        ..Default::default()
+    };
     eframe::run_native(
         "G-Code Editor",
         options,
